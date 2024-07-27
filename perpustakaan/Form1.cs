eddb123163b3ac5aceab06bbbe2108a0f63b6f27
titoagung27 <titofortiktok@gmail.com>
2024-07-27 10:58:16 +0700

    Commit pertama

---
+++ perpustakaan/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace perpustakaan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string nama = "aku";
+            int a = 6;
+            int b = 5;
+            int c = a * b;
+            MessageBox.Show(c + nama);
+
+            string user = "admin";
+            string pass = "123";
+            if (user == "admin" && pass == "123")
+            {
+                MessageBox.Show("berhasil login");
+            }
+            else
+            {
+                MessageBox.Show("user atau password salah");
+            }
+        }
+    }
+}
